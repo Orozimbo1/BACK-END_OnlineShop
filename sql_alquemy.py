84fcf79f167a19a2fd782a8d6bb728de8c2b91fe
Matheus Orozimbo <102329940+Orozimbo1@users.noreply.github.com>
2022-10-08 18:51:13 -0300

    .

--- sql_alquemy.py
+++ sql_alquemy.py
@@ -3,6 +3,7 @@ import dotenv
 import os
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 engine = sqlalchemy.create_engine(os.getenv("DATABASE_URL"))
