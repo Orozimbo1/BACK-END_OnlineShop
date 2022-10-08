6e1aaef3ef11f5351507395f128c172712e9fc89
Matheus Orozimbo <102329940+Orozimbo1@users.noreply.github.com>
2022-10-08 17:42:45 -0300

    .

--- sql_alquemy.py
+++ sql_alquemy.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-engine = sqlalchemy.create_engine('postgresql://{}:{}@{}/{}'.format(os.getenv("USUARIO"),os.getenv("SENHA"),os.getenv("HOST"),os.getenv("DB")))
+engine = sqlalchemy.create_engine('postgresql://{}:{}@{}:{}/{}'.format(os.getenv("USUARIO"),os.getenv("SENHA"),os.getenv("HOST"),os.getenv("PORT"), os.getenv("DB")))
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
