690b7ded65c7c1466dd3daa3c88ba5d7c15d095d
Rafael Felipe <rfaeldel@outlook.com>
2022-10-11 14:16:24 -0300

    conexão com banco de dados

--- sql_alquemy.py
+++ sql_alquemy.py
@@ -3,10 +3,9 @@ import dotenv
 import os
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-engine = sqlalchemy.create_engine(os.getenv("DATABASE_URL"))
+engine = sqlalchemy.create_engine('postgresql://{}:{}@{}/{}'.format(os.getenv("USUARIO"),os.getenv("SENHA"),os.getenv("HOST"),os.getenv("PORTA"),os.getenv("DB")))
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
