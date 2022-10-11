44be3f5fd727f9a7f3004dcea3d6eddf76d4ec99
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2022-10-11 11:30:31 -0300

    Alterações

--- sql_alquemy.py
+++ sql_alquemy.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-engine = sqlalchemy.create_engine(os.getenv("DATABASE_URL"))
+engine = sqlalchemy.create_engine("postgresql://fbweefymqxihya:83e3c84c8d71837369dfb8aedc40a39aad0a00e73011e5b43e380e4f094803ab@ec2-44-210-228-110.compute-1.amazonaws.com:5432/d3i3329k5l69u8")
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
