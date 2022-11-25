c046ed091eb0c27363c69524eb1af78745427900
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2022-11-25 13:30:03 -0300

    imprementando o docker

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 import sqlalchemy
-import dotenv
-import os
+# import dotenv
+# import os
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-dotenv.load_dotenv(dotenv.find_dotenv())
+# dotenv.load_dotenv(dotenv.find_dotenv())
 
-engine = sqlalchemy.create_engine('postgresql://{}:{}@{}/{}'.format(os.getenv("USUARIO"),os.getenv("SENHA"),os.getenv("HOST"),os.getenv("DB")))
+# engine = sqlalchemy.create_engine('postgresql://{}:{}@{}/{}'.format(os.getenv("USUARIO"),os.getenv("SENHA"),os.getenv("HOST"),os.getenv("DB")))
+engine = sqlalchemy.create_engine('postgresql://postgres:postgres@localhost/OnlineShop')
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
