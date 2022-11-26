6c65ea0dbac281e7a6ccf2e4531ede71bf222155
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2022-11-26 14:37:25 -0300

    .

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import sqlalchemy
-# import dotenv
-# import os
+import dotenv
+import os
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# dotenv.load_dotenv(dotenv.find_dotenv())
+dotenv.load_dotenv(dotenv.find_dotenv())
 
 engine = sqlalchemy.create_engine('postgresql://{}:{}@{}:{}/{}'.format(os.getenv("USUARIO"),os.getenv("SENHA"),os.getenv("HOST"),os.getenv("PORTA"),os.getenv("DB")))
 Base = declarative_base()
