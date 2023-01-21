2a73021ca4529c1443fca159c0d5ae6658bf40f1
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-01-21 19:23:23 -0300

    configurando

--- app/app.py
+++ app/app.py
@@ -115,4 +115,4 @@ api.add_resource(Secao, '/secao/<int:secao_produto_id>')
 api.add_resource(SecaoCadastro, '/secao/cadastro')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port="5000")
+    app.run(debug=True, port="5000")
